3999a8aa8a278d0d106ca2efeda3626769a440ac
Jacques Feruzi <jferuzi019@gmail.com>
2024-07-22 16:45:30 +0200

    listview

--- ToDo/app1/lib/main.dart
+++ ToDo/app1/lib/main.dart
@@ -17,8 +17,34 @@ class MyWidget extends StatelessWidget {
               ),
               backgroundColor: const Color.fromARGB(255, 77, 225, 134),
             ),
-            body: const Center(
-              child: Text("Hello jacques"),
-            )));
+            body: Center(
+                child: Container(
+                    color: Colors.deepPurpleAccent,
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: ListView(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: Center(child: Text('sikatenda')),
+                            decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: BorderRadius.circular(12.0)),
+                            height: 100,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            child: Center(child: Text('Feruzi')),
+                            decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: BorderRadius.circular(12.0)),
+                            height: 100,
+                          ),
+                        )
+                      ],
+                    )))));
   }
 }
