eb5428f8e34ce5166851d063a956c8b3f5bfa6cb
Jacques Feruzi <jferuzi019@gmail.com>
2024-07-29 15:24:54 +0200

    list icons

--- ToDo/app1/lib/listItem.dart
+++ ToDo/app1/lib/listItem.dart
@@ -4,7 +4,11 @@ class MyList extends StatelessWidget {
   final String name;
   final String completed;
 
-  const MyList({super.key, required this.name, required this.completed});
+  const MyList({
+    super.key,
+    required this.name,
+    required this.completed,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,26 +17,32 @@ class MyList extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
             color: Colors.amber, borderRadius: BorderRadius.circular(12.0)),
-        height: 100,
+        height: 110,
         width: 270,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 12.0, top: 8.0),
               child: Text(name, style: const TextStyle(fontSize: 21)),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 12.0),
-              child: Row(
-                children: [
-                  Text(completed, style: const TextStyle(fontSize: 16)),
-                  const SizedBox(
-                    width: 180,
-                  ),
-                  IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
-                ],
+              padding: const EdgeInsets.only(
+                left: 12.0,
               ),
+              child: Text(completed, style: const TextStyle(fontSize: 16)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.edit_rounded)),
+                const SizedBox(
+                  width: 100,
+                ),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+              ],
             ),
           ],
         ),
